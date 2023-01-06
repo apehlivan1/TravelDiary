fc7bb63444b63dd20f5c2bc13ea16a9d18e6e568
apehlivan1 <apehlivan1@etf.unsa.ba>
2023-01-06 22:47:56 +0100

    specify controller inside the FXML file

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -14,8 +14,6 @@ public class AppFX extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/welcome-page.fxml"));
-        WelcomePage welcomePageController = new WelcomePage();
-        fxmlLoader.setController(welcomePageController);
         Parent root = fxmlLoader.load();
         primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         primaryStage.setResizable(false);
