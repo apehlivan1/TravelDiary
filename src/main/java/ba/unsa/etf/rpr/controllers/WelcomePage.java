eb12221ed689b05b92bbaa5749456839aac9c482
apehlivan1 <apehlivan1@etf.unsa.ba>
2023-01-06 22:03:22 +0100

    add welcome page controller

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/WelcomePage.java
@@ -0,0 +1,31 @@
+package ba.unsa.etf.rpr.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class WelcomePage {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void btnGuestClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogInClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
