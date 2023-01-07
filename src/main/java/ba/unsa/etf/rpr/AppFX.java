b8312c2e32a4d071ee31c32420c9d88da96ea617
apehlivan1 <apehlivan1@etf.unsa.ba>
2023-01-07 02:53:20 +0100

    small changes

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -13,10 +13,10 @@ public class AppFX extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/welcome-page.fxml"));
-        Parent root = fxmlLoader.load();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/welcome-page.fxml"));
         primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         primaryStage.setResizable(false);
+        primaryStage.setTitle("Explore Bosnia");
         primaryStage.show();
     }
     public static void main(String[] args) { launch(args); }
