a3f18b2f43093b77f0dcd4ea26b1e9c50e310b4c
apehlivan1 <apehlivan1@etf.unsa.ba>
2023-03-02 15:47:36 +0100

    add Destination class

--- src/main/java/ba/unsa/etf/rpr/domain/Destination.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Destination.java
@@ -1,4 +1,61 @@
 package ba.unsa.etf.rpr.domain;
 
-public class Destination {
+public class Destination implements Idable {
+    int id;
+    String name;
+    String location;
+    String description;
+    int rating;
+
+    public Destination(int id, String name, String location, String description, int rating) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.description = description;
+        this.rating = rating;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int value) {
+        this.id = value;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
 }
