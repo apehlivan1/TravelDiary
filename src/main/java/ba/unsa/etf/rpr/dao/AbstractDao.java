b045649a644cb14803256b15ce01c6c1e8b3e434
apehlivan1 <apehlivan1@etf.unsa.ba>
2023-07-28 21:51:52 +0200

    add AbstractDao

---
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -0,0 +1,70 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Idable;
+import ba.unsa.etf.rpr.exceptions.AppException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+public abstract class AbstractDao<T extends Idable> implements Dao<T> {
+    private static Connection connection = null;
+    private String tableName;
+
+    public AbstractDao(String tableName) {
+        this.tableName = tableName;
+        try {
+            Properties p = new Properties();
+            p.load(ClassLoader.getSystemResource("db.properties").openStream());
+            String url = p.getProperty("url");
+            String username = p.getProperty("username");
+            String password = p.getProperty("password");
+            AbstractDao.connection = DriverManager.getConnection(url, username, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }));
+        }
+    }
+
+    public static Connection getConnection(){
+        return AbstractDao.connection;
+    }
+
+    public abstract T row2object(ResultSet rs) throws AppException;
+
+    public abstract Map<String, Object> object2row(T object);
+
+
+    public T add(T item) throws AppException {
+        return null;
+    }
+
+    public T update(T item) throws AppException {
+        return null;
+    }
+
+    public T getById(int id) throws AppException {
+        return null;
+    }
+
+    public void delete(int id) throws AppException {
+
+    }
+
+    public List<T> getAll() throws AppException {
+        return null;
+    }
+
+
+}
