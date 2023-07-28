48f67aeda727dd791baf1cc1da2fccab2a6789fc
apehlivan1 <apehlivan1@etf.unsa.ba>
2023-07-28 17:49:37 +0200

    add Category implementation

--- src/main/java/ba/unsa/etf/rpr/domain/Category.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Category.java
@@ -1,4 +1,39 @@
 package ba.unsa.etf.rpr.domain;
 
-public class Category {
+import java.util.Objects;
+
+public class Category implements Idable {
+
+    private int id;
+    private String name;
+
+    public Category() {}
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int value) {
+        id = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
