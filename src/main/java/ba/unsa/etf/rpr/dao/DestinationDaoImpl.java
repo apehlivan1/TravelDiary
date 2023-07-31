833f4acd2b60a4e564367d6705debac963170a4c
apehlivan1 <apehlivan1@etf.unsa.ba>
2023-07-31 18:57:33 +0200

    DestinationDaoImpl implementation

--- src/main/java/ba/unsa/etf/rpr/dao/DestinationDaoImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DestinationDaoImpl.java
@@ -1,4 +1,45 @@
 package ba.unsa.etf.rpr.dao;
 
-public class DestinationDaoImpl {
+import ba.unsa.etf.rpr.domain.Destination;
+import ba.unsa.etf.rpr.exceptions.AppException;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class DestinationDaoImpl extends AbstractDao<Destination> implements DestinationDao {
+
+    private DestinationDaoImpl() {
+        super("destinations");
+    }
+
+    @Override
+    public Destination row2object(ResultSet rs) throws AppException {
+        try {
+            Destination destination = new Destination(
+                    rs.getInt("id"),
+                    rs.getString("name"),
+                    rs.getString("location"),
+                    rs.getString("description"),
+                    rs.getInt("categoryId"), //try using DaoFactory.categoryDao() method to get by id
+                    rs.getInt("rating")
+            );
+            return destination;
+        } catch (SQLException e) {
+            throw new AppException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public Map<String, Object> object2row(Destination object) {
+        Map<String, Object> row = new TreeMap<String, Object>();
+        row.put("id", object.getId());
+        row.put("name", object.getName());
+        row.put("location", object.getLocation());
+        row.put("description", object.getDescription());
+        row.put("categoryId", object.getCategoryId());
+        row.put("rating", object.getRating());
+        return row;
+    }
 }
