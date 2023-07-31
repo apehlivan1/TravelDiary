933f77abd8f5800f00051d6ea1198db787555a12
apehlivan1 <apehlivan1@etf.unsa.ba>
2023-08-01 01:59:03 +0200

    implementation of DaoFactory

--- src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -1,4 +1,16 @@
 package ba.unsa.etf.rpr.dao;
 
 public class DaoFactory {
+
+    private static final CategoryDao categoryDao = CategoryDaoImpl.getInstance();
+    private static final DestinationDao destinationDao = DestinationDaoImpl.getInstance();
+    private static final TripDao tripDao = TripDaoImpl.getInstance();
+    private static final UserDao userDao = UserDaoImpl.getInstance();
+
+    private DaoFactory() {}
+
+    private static CategoryDao getCategoryDao() { return categoryDao; }
+    private static DestinationDao getDestinationDao() { return destinationDao; }
+    private static TripDao getTripDao() { return tripDao; }
+    private static UserDao getUserDao() { return userDao; }
 }
