9dc7d2758ee95a55c2c1001d19ce9988b02446d1
apehlivan1 <apehlivan1@etf.unsa.ba>
2023-08-02 23:39:14 +0200

    add RegisterController

--- src/main/java/ba/unsa/etf/rpr/controllers/RegisterController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegisterController.java
@@ -1,4 +1,55 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class RegisterController {
+
+    @FXML
+    private Label blackMessageLabel;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private Label greenMessageLabel;
+
+    @FXML
+    private PasswordField passwordPasswordField;
+
+    @FXML
+    private Label redMessageLabel;
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    void cancelClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void passwordOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registerClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void usernameOnAction(ActionEvent event) {
+
+    }
+
 }
+
+
