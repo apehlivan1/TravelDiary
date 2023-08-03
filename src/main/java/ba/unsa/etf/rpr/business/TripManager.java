6b4145234808929097f2f2d0581dd834ae7750b3
apehlivan1 <apehlivan1@etf.unsa.ba>
2023-08-03 19:44:54 +0200

    implementation of TripManager

--- src/main/java/ba/unsa/etf/rpr/business/TripManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/TripManager.java
@@ -1,4 +1,31 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Trip;
+import ba.unsa.etf.rpr.exceptions.AppException;
+
+import java.util.List;
+
 public class TripManager {
+
+    public Trip add(Trip trip) throws AppException {
+        return DaoFactory.getTripDao().add(trip);
+    }
+
+    public Trip update(Trip trip) throws AppException {
+        return DaoFactory.getTripDao().update(trip);
+    }
+
+    public Trip getById(int id) throws AppException {
+        return DaoFactory.getTripDao().getById(id);
+    }
+
+    public void delete(int id) throws AppException {
+        DaoFactory.getTripDao().delete(id);
+    }
+
+    public List<Trip> getAll() throws AppException {
+        return DaoFactory.getTripDao().getAll();
+    }
+
 }
