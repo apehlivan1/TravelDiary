302a1297fb7f28911664546b15ac8e321fd33c8c
apehlivan1 <apehlivan1@etf.unsa.ba>
2023-08-03 22:58:30 +0200

    add basic methods to DestinationManager

--- src/main/java/ba/unsa/etf/rpr/business/DestinationManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/DestinationManager.java
@@ -1,4 +1,31 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Destination;
+import ba.unsa.etf.rpr.domain.Trip;
+import ba.unsa.etf.rpr.exceptions.AppException;
+
+import java.util.List;
+
 public class DestinationManager {
+
+    public Destination add(Destination destination) throws AppException {
+        return DaoFactory.getDestinationDao().add(destination);
+    }
+
+    public Destination update(Destination destination) throws AppException {
+        return DaoFactory.getDestinationDao().update(destination);
+    }
+
+    public Destination getById(int id) throws AppException {
+        return DaoFactory.getDestinationDao().getById(id);
+    }
+
+    public void delete(int id) throws AppException {
+        DaoFactory.getDestinationDao().delete(id);
+    }
+
+    public List<Destination> getAll() throws AppException {
+        return DaoFactory.getDestinationDao().getAll();
+    }
 }
