eb31473b05d605fbe5c3b1d4a557c61a27eaf1a7
apehlivan1 <apehlivan1@etf.unsa.ba>
2023-08-03 18:52:10 +0200

    home page controller

--- src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -1,4 +1,46 @@
 package ba.unsa.etf.rpr.controllers;
 
+        import java.net.URL;
+        import java.util.ResourceBundle;
+        import javafx.event.ActionEvent;
+        import javafx.fxml.FXML;
+        import javafx.scene.control.Button;
+        import javafx.scene.control.TextField;
+
 public class HomeController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button exploreBtn;
+
+    @FXML
+    private Button searchBtn;
+
+    @FXML
+    private TextField searchTextField;
+
+    @FXML
+    void exploreBtnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert exploreBtn != null : "fx:id=\"exploreBtn\" was not injected: check your FXML file 'home.fxml'.";
+        assert searchBtn != null : "fx:id=\"searchBtn\" was not injected: check your FXML file 'home.fxml'.";
+        assert searchTextField != null : "fx:id=\"searchTextField\" was not injected: check your FXML file 'home.fxml'.";
+
+    }
+
 }
+
