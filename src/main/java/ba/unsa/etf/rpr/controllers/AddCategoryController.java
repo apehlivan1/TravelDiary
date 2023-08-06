64e669d7c11ba966444ad5318928de7557d54107
apehlivan1 <apehlivan1@etf.unsa.ba>
2023-08-06 03:39:40 +0200

    add destination button enabled

--- src/main/java/ba/unsa/etf/rpr/controllers/AddCategoryController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AddCategoryController.java
@@ -2,8 +2,16 @@ package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class AddCategoryController {
 
@@ -23,8 +31,14 @@ public class AddCategoryController {
     }
 
     @FXML
-    void addDestinationClicked(ActionEvent event) {
+    void addDestinationClicked(ActionEvent event) throws IOException {
+        Stage stage = (Stage) addDestinationBtn.getScene().getWindow();
+        stage.close();
 
+        stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/add destination.fxml"));
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.show();
     }
 
     @FXML
