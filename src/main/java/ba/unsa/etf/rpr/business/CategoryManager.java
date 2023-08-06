e00cbc19bf1d629f8a3e0802ba28559a3454f380
apehlivan1 <apehlivan1@etf.unsa.ba>
2023-08-06 04:16:35 +0200

    add basic methods to CategoryManager

--- src/main/java/ba/unsa/etf/rpr/business/CategoryManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/CategoryManager.java
@@ -1,4 +1,31 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Category;
+import ba.unsa.etf.rpr.exceptions.AppException;
+
+import java.util.List;
+
 public class CategoryManager {
+
+    public Category add(Category category) throws AppException {
+        return DaoFactory.getCategoryDao().add(category);
+    }
+
+    public Category update(Category category) throws AppException {
+        return DaoFactory.getCategoryDao().update(category);
+    }
+
+    public Category getById(int id) throws AppException {
+        return DaoFactory.getCategoryDao().getById(id);
+    }
+
+    public void delete(int id) throws AppException {
+        DaoFactory.getCategoryDao().delete(id);
+    }
+
+    public List<Category> getAll() throws AppException {
+        return DaoFactory.getCategoryDao().getAll();
+    }
+
 }
