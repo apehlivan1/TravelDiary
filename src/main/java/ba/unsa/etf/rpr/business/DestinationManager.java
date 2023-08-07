5f36294130c71ae2a5e55bbc3eccbf8f758f51dd
apehlivan1 <apehlivan1@etf.unsa.ba>
2023-08-07 22:06:51 +0200

    add searchByCategory to the DestinationManager

--- src/main/java/ba/unsa/etf/rpr/business/DestinationManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/DestinationManager.java
@@ -2,7 +2,6 @@ package ba.unsa.etf.rpr.business;
 
 import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Destination;
-import ba.unsa.etf.rpr.domain.Trip;
 import ba.unsa.etf.rpr.exceptions.AppException;
 
 import java.util.List;
@@ -31,5 +30,9 @@ public class DestinationManager {
 
     public List<Destination> search(String text) throws AppException {
         return DaoFactory.getDestinationDao().search(text);
+    }
+
+    public List<Destination> searchByCategory(int id) throws AppException {
+        return DaoFactory.getDestinationDao().searchByCategory(id);
     }
 }
