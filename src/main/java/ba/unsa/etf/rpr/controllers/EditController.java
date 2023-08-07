50816fe264fac492378a7674831469c67735b65a
apehlivan1 <apehlivan1@etf.unsa.ba>
2023-08-07 23:59:17 +0200

    editController implementation

--- src/main/java/ba/unsa/etf/rpr/controllers/EditController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/EditController.java
@@ -1,4 +1,40 @@
 package ba.unsa.etf.rpr.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class EditController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    void cancelClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert cancelButton != null : "fx:id=\"cancelButton\" was not injected: check your FXML file 'edit.fxml'.";
+        assert saveButton != null : "fx:id=\"saveButton\" was not injected: check your FXML file 'edit.fxml'.";
+
+    }
+
 }
