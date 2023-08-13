c9e8545b3545aa5eb615d7fad736a90231bd28e0
apehlivan1 <apehlivan1@etf.unsa.ba>
2023-08-13 03:31:14 +0200

    add javadoc for WelcomePageController

--- src/main/java/ba/unsa/etf/rpr/controllers/WelcomePageController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/WelcomePageController.java
@@ -1,8 +1,5 @@
 package ba.unsa.etf.rpr.controllers;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,17 +7,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
+/**
+ * Controller class for the welcome page of the application.
+ *
+ * @author Almedina Pehlivan
+ */
 public class WelcomePageController {
-
-    @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
 
     @FXML
     private Button logInBtn;
@@ -28,30 +25,43 @@ public class WelcomePageController {
     @FXML
     private Button registerBtn;
 
-
+    /**
+     * Handles the event when the register button is clicked.
+     * Opens the registration screen and closes the current welcome page.
+     *
+     * @param event The action event triggered by clicking the register button.
+     * @throws IOException newStage method exception
+     */
     @FXML
     void btnRegisterClicked(ActionEvent event) throws IOException {
         newStage("/fxml/register.fxml");
         ((Stage) registerBtn.getScene().getWindow()).close();
     }
 
+    /**
+     * Handles the event when the login button is clicked.
+     * Opens the login screen and closes the current welcome page.
+     *
+     * @param event The action event triggered by clicking the login button.
+     * @throws IOException If an I/O error occurs while loading the login screen.
+     */
     @FXML
     void btnLogInClicked(ActionEvent event) throws IOException {
         newStage("/fxml/login.fxml");
         ((Stage) logInBtn.getScene().getWindow()).close();
     }
 
-    @FXML
-    void initialize() {
-
-    }
-
+    /**
+     * Opens a new scene with the specified FXML resource.
+     *
+     * @param resource The path to the FXML resource file to be loaded.
+     * @throws IOException If an I/O error occurs while loading the FXML resource.
+     */
     private void newStage(String resource) throws IOException {
         Stage stage = new Stage();
         Parent root = FXMLLoader.load(getClass().getResource(resource));
         stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         stage.setResizable(false);
-        //stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
     }
 
